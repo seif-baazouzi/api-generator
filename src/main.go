917eedaecc66fc41b9f4cb6c2c034a2bc054ea0a
seif <baazouziseif9@gmail.com>
2022-10-15 11:47:10 +0100

    create dist dir

--- src/main.go
+++ src/main.go
@@ -5,21 +5,41 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 func main() {
+	config := parseConfigFile()
+	clearDistDirectory()
+	fmt.Println(config)
+}
+
+func parseConfigFile() config.Config {
 	flag.Parse()
 	args := flag.Args()
 
 	if len(args) == 0 {
-		usage()
+		fmt.Fprintf(os.Stderr, "Usage: ./main [config-file]\n")
+		os.Exit(1)
 	}
 
 	configPath := args[0]
-	fmt.Println(config.Parse(configPath))
+	config, err := config.Parse(configPath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Can not read config file %s\n", configPath)
+		os.Exit(1)
+	}
+
+	return config
 }
 
-func usage() {
-	fmt.Fprintf(os.Stderr, "Usage: ./main [config-file]\n")
-	os.Exit(1)
+func clearDistDirectory() {
+	distPath := filepath.Join(".", "dist")
+	os.RemoveAll(distPath)
+	err := os.MkdirAll(distPath, os.ModePerm)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Could not create dist directory\n")
+		os.Exit(1)
+	}
 }
