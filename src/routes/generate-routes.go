a5fef178c0f7fd8e3b47fefbb55caa271683aa4f
seif <baazouziseif9@gmail.com>
2022-10-18 13:44:48 +0100

    generate main file from template

--- src/routes/generate-routes.go
+++ src/routes/generate-routes.go
@@ -2,53 +2,34 @@ package routes
 
 import (
 	"api-generator/src/config"
+	"api-generator/src/utils"
 	"fmt"
+	"html/template"
+	t "html/template"
 	"os"
 	"path/filepath"
 	"strings"
 )
 
 func GenerateRoutes(config config.Config) {
-	routesCode := strings.Join([]string{
-		`require("dotenv").config()`,
-		``,
-		`const express = require("express")`,
-		`const app = express()`,
-		`const cors = require("cors")`,
-		`const { Pool } = require("pg")`,
-		``,
-		`app.use(cors())`,
-		`app.use(require("body-parser").json())`,
-		``,
-		`const db = new Pool({`,
-		`	user: process.env.PG_USER,`,
-		`	password: process.env.PG_PASS,`,
-		`	host: process.env.PG_HOST,`,
-		`	database: process.env.PG_DB,`,
-		`	max: 10,`,
-		`})`,
-		``,
-		`db.on("error", (err, client) => {`,
-		`	console.error("Unexpected error on idle client", err)`,
-		`	process.exit(-1)`,
-		`})`,
-		``,
-		`console.log("Connected to postgresql database")`,
-		``,
-	}, "\n")
+	routesCode := strings.Join([]string{}, "\n")
 
 	for collectionName, collection := range config.Collections {
 		routesCode += generatorCollectionRoutes(collectionName, collection)
 	}
 
-	routesCode += strings.Join([]string{
-		`const PORT = process.env.PORT || 3000`,
-		`app.listen(PORT, () => console.log("Server started on port " + PORT))`,
-		``,
-	}, "\n")
+	templatePath := filepath.Join(".", "templates", "main.temp")
+	template, err := template.ParseFiles(templatePath)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Could not parse template %s\n", templatePath)
+		os.Exit(1)
+	}
+
+	code, err := utils.TemplateToString(template, t.HTML(routesCode))
 
 	filePath := filepath.Join(".", "dist", "server.js")
-	err := os.WriteFile(filePath, []byte(routesCode), os.ModePerm)
+	err = os.WriteFile(filePath, []byte(code), os.ModePerm)
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Could not create %s file\n", filePath)
@@ -58,11 +39,9 @@ func GenerateRoutes(config config.Config) {
 
 func generatorCollectionRoutes(collectionName string, collection config.Collection) string {
 	return strings.Join([]string{
-		``,
 		generatorGetRoute(collectionName),
 		generatorPostRoute(collectionName, collection),
 		generatorPutRoute(collectionName, collection),
 		generatorDeleteRoute(collectionName),
-		``,
 	}, "\n")
 }
